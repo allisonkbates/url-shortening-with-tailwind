c15b47361a24469447234325289010004ef72c99
Allison <allisonkbates@gmail.com>
2020-07-22 22:25:59 -0400

    first commit

---
+++ components/nav.js
@@ -0,0 +1,24 @@
+import Link from 'next/link'
+
+const links = [
+	{ href: '#', label: 'GitHub' },
+	{ href: '#', label: 'Docs' },
+	{ href: '#', label: ''}
+]
+
+export default function Nav() {
+	return (
+    <nav className="flex p-8 md:py-16 md:px-20 lg:px-40 space-x-4 justify-between">
+      <ul className="flex space-x-4">	
+        <img src="/logo.svg" className="mr-4 object-contain"></img>
+        <li className="hidden md:flex items-center text-light-gray font-bold"><a>Features</a></li>
+        <li className="hidden md:flex items-center text-light-gray font-bold"><a>Pricing</a></li>
+        <li className="hidden md:flex items-center text-light-gray font-bold"><a>Resources</a></li>
+      </ul>
+      <ul className="flex space-x-4">
+        <li className="hidden md:flex items-center text-light-gray font-bold"><a>Login</a></li>
+        <li className="hidden md:flex items-center text-white font-bold bg-bright-teal py-2 px-6 rounded-full"><a>Sign Up</a></li>
+      </ul>
+    </nav>
+  )
+}
